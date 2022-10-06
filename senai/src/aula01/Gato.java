1042a9c014b1b95df73cc49121c19ac904e7335d
isaacgoncod <isaacgoncod@gmail.com>
2022-10-06 11:21:10 -0300

    aula01 oop

---
+++ senai/src/aula01/Gato.java
@@ -0,0 +1,15 @@
+package aula01;
+
+public class Gato {
+	
+	String[] nome = new String[3];
+	String[] raca = new String[3];
+	double[] peso = new double[3];
+	
+	void miar() {
+		System.out.println("miaumiau");
+	}
+	void ronronar() {
+		System.out.println("ronronron");
+	}
+}
