86861b2e4b08dab3e74aec77c787de479bc5900d
isaacgoncod <isaacgoncod@gmail.com>
2022-11-11 11:18:43 -0300

    aula17-exercicios

--- senai/src/aula17/exercicios/eleicao/Main.java
+++ senai/src/aula17/exercicios/eleicao/Main.java
@@ -1,6 +1,8 @@
 package aula17.exercicios.eleicao;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
@@ -11,7 +13,15 @@ public class Main {
 
 	static int menuG = 0;
 	static int menuP = 0;
-
+	static int contGt = 0;
+	static int contGf = 0;
+	static int contGb = 0;
+	static int contGn = 0;
+	static int contPl = 0;
+	static int contPj = 0;
+	static int contPb = 0;
+	static int contPn = 0;
+	
 	public static void main(String[] args) {
 
 		Eleicao votos = new Eleicao();
@@ -36,8 +46,9 @@ public class Main {
 
 			System.out.println("CONFIRMAR?\n1-SIM\n0-NAO");
 			menuG = scan.nextInt();
-			
-			if(menuG == 1) System.out.println("CONFIRMADO");
+
+			if (menuG == 1)
+				System.out.println("CONFIRMADO");
 		}
 
 		while (menuP != 1) {
@@ -49,8 +60,9 @@ public class Main {
 
 			System.out.println("CONFIRMAR?\n1-SIM\n0-NAO");
 			menuP = scan.nextInt();
-			
-			if(menuG == 1) System.out.println("CONFIRMADO");
+
+			if (menuG == 1)
+				System.out.println("CONFIRMADO");
 		}
 
 		try {
@@ -61,10 +73,42 @@ public class Main {
 
 			System.out.println("FIM!");
 
+			BufferedReader br = new BufferedReader(
+					new FileReader("./src/aula17/exercicios/eleicao/bancodados/eleicao.csv"));
+			String s = null;
+
+			while ((s = br.readLine()) != null) {
+				if (s.equals("TARCISIO GOMES DE FREITAS")) {
+					contGt++;
+				}
+				if (s.equals("FERNANDO HADDAD")) {
+					contGf++;
+				}
+				if (s.equals("GOVERNADOR : VOTO BRANCO")) {
+					contGb++;
+				}
+				if (s.equals("GOVERNADOR : VOTO NULO")) {
+					contGn++;
+				}
+				if (s.equals("LUIZ INACIO LULA DA SILVA")) {
+					contPl++;
+				}
+				if (s.equals("JAIR MESSIAS BOLSONARO")) {
+					contPj++;
+					System.out.println("JAIR MESSIAS BOLSONARO: " + contPj);
+				}
+				if (s.equals("PRESIDENTE : VOTO BRANCO")) {
+					contPb++;
+				}
+				if (s.equals("PRESIDENTE : VOTO NULO")) {
+					contPn++;
+				}
+			}
+			br.close();
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
 	}
 
 }
